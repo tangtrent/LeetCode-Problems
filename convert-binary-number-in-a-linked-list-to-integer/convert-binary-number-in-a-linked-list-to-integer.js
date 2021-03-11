2ddf9961a7fedeb174513408ade871011d346264
Trent Tang <tangtrent@gmail.com>
2021-03-11 10:01:23 -0800

    Time: 88 ms (19.00%), Space: 38.5 MB (84.14%) - LeetHub

---
+++ convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    let result = [];
+    while (head) {
+        result.push(head.val);
+        
+        head = head ? head.next : undefined;
+    }
+    
+    return parseInt(result.join(''), 2);
+};
